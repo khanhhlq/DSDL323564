0f9871af9f9cc4af62dd60b1257ed5736488c479
khanhhlq <lee.quockhanh1107@gmail.com>
2026-08-25 21:38:19 +0700

    Add expected output and PASS/FAIL checking to testbench

--- SS1/half-adder/testbench.v
+++ SS1/half-adder/testbench.v
@@ -14,3 +14,20 @@ module half_adder_tb;
         a = 1; b = 1;
     end
 endmodule
+
+//// TESTBENCH 
+// module half_adder_tb;
+//     reg a, b;
+//     wire s, c_out;
+
+//     half_adder ha(a, b, s, c_out);
+
+//     initial begin
+//         a=0; b=0; #10; if({s,c_out}==2'b00) $display("PASS"); else $display("FAIL");
+//         a=0; b=1; #10; if({s,c_out}==2'b10) $display("PASS"); else $display("FAIL");
+//         a=1; b=0; #10; if({s,c_out}==2'b10) $display("PASS"); else $display("FAIL");
+//         a=1; b=1; #10; if({s,c_out}==2'b01) $display("PASS"); else $display("FAIL");
+
+//         $finish;
+//     end
+// endmodule
