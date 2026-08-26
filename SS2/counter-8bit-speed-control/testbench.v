d2479381641b7a6884d2ef57b7eb75d47c4b3a3c
khanhhlq <lee.quockhanh1107@gmail.com>
2026-08-26 21:54:57 +0700

    Edit comment testbench.v

--- SS2/counter-8bit-speed-control/testbench.v
+++ SS2/counter-8bit-speed-control/testbench.v
@@ -10,24 +10,24 @@ module DEM_8bit_tb;
     end
 
     initial begin
-        // Trạng thái ban đầu
+        // Initial
         reset = 1; speed = 0; UD = 1; SS = 0;
         #40;
         reset = 0;
         
-        // Test 1: Cho đếm lên với SPEED = 0 (5MHz)
+        // Test 1: Up counting (5MHz)
         SS = 1; UD = 1; speed = 0;
         #2000;
 
-        // Test 2: Dừng đếm (SS = 0)
+        // Test 2: Stop counting
         SS = 0;
         #500;
 
-        // Test 3: Đổi hướng thành đếm xuống (UD = 0) và tiếp tục cho đếm (SS = 1)
+        // Test 3: Down counting (5MHz)
         UD = 0; SS = 1;
         #2000;
 
-        // Test 4: Đổi tốc độ sang SPEED = 1 (1MHz) và tiếp tục đếm lên (UD = 1)
+        // Test 4: Up couting (1MHz)
         speed = 1; UD = 1;
         #6000;
         $finish;
